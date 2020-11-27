bd00bafbb4118e5b25844820e2b263460d80e1b8
Jessica Cavalcante <je-cavalcante@hotmail.com>
2020-11-27 18:53:27 -0300

    add informations in pokedex.html

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,6 +1,5 @@
 import data from './data/pokemon/pokemon.js';
 import {getPokemonIndexFromUrl} from './data.js';
-
 getPokemonIndexFromUrl();
 
 //console.log(document.getElementsByClassName('name-pokemon')[0]);
@@ -8,9 +7,10 @@ getPokemonIndexFromUrl();
 const pokemon = data.pokemon[getPokemonIndexFromUrl()];
 
 document.getElementsByClassName('name-pokemon')[0].innerHTML = pokemon.name;
-document.getElementsByClassName('pokedex-pokemon-img')[0].src = pokemon.img;
-document.getElementsByClassName('pokedex-list-about')[0].innerHTML = pokemon.about;
-document.getElementsByClassName('pokedex-list-type')[0].innerHTML = pokemon.type;
-document.getElementsByClassName('pokedex-list-weaknesses')[0].innerHTML = pokemon.weaknesses;
-document.getElementsByClassName('pokedex-list-resistant')[0].innerHTML = pokemon.resistant;
+document.getElementsByClassName('pokedex-pokemon-img')[0].src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'+ pokemon.num + '.png';
+document.getElementsByClassName('pokedex-list-about')[0].innerHTML = 'About:' + pokemon.about;
+document.getElementsByClassName('pokedex-list-type')[0].innerHTML = 'Type:' + pokemon.type;
+document.getElementsByClassName('pokedex-list-weaknesses')[0].innerHTML = 'Weaknesses:' + pokemon.weaknesses;
+document.getElementsByClassName('pokedex-list-resistant')[0].innerHTML ='Resistant:' + pokemon.resistant;
+document.getElementsByClassName('pokedex-list-km')[0].innerHTML ='Km Egg:' + pokemon.egg;
 
