6799c0f44b8c23bc78d4b3660fcf3344b70f9807
Jessica Cavalcante <je-cavalcante@hotmail.com>
2020-12-01 17:50:23 -0300

    alteração no html e uso do dom no pokedex.js

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,16 +1,76 @@
 import data from './data/pokemon/pokemon.js';
-import {getPokemonIndexFromUrl} from './data.js';
-getPokemonIndexFromUrl();
+import {getPokemonNumFromUrl, getPokemonByNum, computeStats} from './data.js';
 
-//console.log(document.getElementsByClassName('name-pokemon')[0]);
+const pokemon = getPokemonByNum(data.pokemon, getPokemonNumFromUrl());
 
-const pokemon = data.pokemon[getPokemonIndexFromUrl()];
+console.log(computeStats(pokemon['special-attack']));
+const bestAttack = computeStats(pokemon['special-attack'], true);
+const worstAttack = computeStats(pokemon['special-attack'], false);
 
-document.getElementsByClassName('name-pokemon')[0].innerHTML = pokemon.name;
-document.getElementsByClassName('pokedex-pokemon-img')[0].src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'+ pokemon.num + '.png';
-document.getElementsByClassName('pokedex-list-about')[0].innerHTML = 'About:' + pokemon.about;
-document.getElementsByClassName('pokedex-list-type')[0].innerHTML = 'Type:' + pokemon.type;
-document.getElementsByClassName('pokedex-list-weaknesses')[0].innerHTML = 'Weaknesses:' + pokemon.weaknesses;
-document.getElementsByClassName('pokedex-list-resistant')[0].innerHTML ='Resistant:' + pokemon.resistant;
-document.getElementsByClassName('pokedex-list-km')[0].innerHTML ='Km Egg:' + pokemon.egg;
+document.getElementById('name-pokemon').innerHTML = pokemon.name;
+document.getElementById('pokedex-pokemon-img').src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'+ pokemon.num + '.png';
+document.getElementById('list-about').innerHTML = '<span class="info-title">Sobre: </span><span class="info-value">' + pokemon.about  + '</span>';
+document.getElementById('list-type').innerHTML = '<span class="info-title">Tipo: </span><span class="info-value">' + pokemon.type  + '</span>';
+document.getElementById('list-weaknesses').innerHTML = '<span class="info-title">Fraquezas: </span><span class="info-value">' + pokemon.weaknesses  + '</span>';
+document.getElementById('list-resistant').innerHTML ='<span class="info-title">Resistência: </span><span class="info-value">' + pokemon.resistant  + '</span>';
+document.getElementById('list-km').innerHTML ='<span class="info-title">Km Egg: </span><span class="info-value">' + pokemon.egg  + '</span>';
+document.getElementById('max-damage').innerHTML ='O maior dano causado é o ' + bestAttack.name + ' e seu valor é de ' + bestAttack['base-damage'] + '.';
+document.getElementById('min-damage').innerHTML ='O menor dano causado é o ' + worstAttack.name + ' e seu valor é de ' + worstAttack['base-damage'] + '.';
 
+function getCard(data) {
+    let img = document.createElement('img');
+    img.src = 'https://www.serebii.net/pokemongo/pokemon/' + data.num +'.png';
+
+    let orderedList = document.createElement('ol');
+    orderedList.classList.add('list-evolution');
+
+    let listInfo1 = document.createElement('li');
+    listInfo1.classList.add('img-evolution');
+    listInfo1.appendChild(img);
+
+    let listInfo2 = document.createElement('li');
+    listInfo2.classList.add('num-evolution');
+    listInfo2.innerHTML = data.num;
+
+    let listInfo3 = document.createElement('li');
+    listInfo3.classList.add('name-evolution');
+    listInfo3.innerHTML = data.name;
+
+    let card = document.createElement('div');
+    card.classList.add('card-evolution');
+    orderedList.appendChild(listInfo1);
+    orderedList.appendChild(listInfo2);
+    orderedList.appendChild(listInfo3);
+    card.appendChild(orderedList);
+    return card;
+}
+
+if(pokemon.evolution['prev-evolution']) {
+    for(const prevEvolution of pokemon.evolution['prev-evolution']) {
+        let card = getCard(prevEvolution);
+        if(prevEvolution['prev-evolution']) {
+            for(const prevEvolution2 of prevEvolution['prev-evolution']) {
+                let card = getCard(prevEvolution2);
+                document.getElementsByClassName('evolution')[0].appendChild(card);
+            }
+        }
+        document.getElementsByClassName('evolution')[0].appendChild(card);
+    }
+}
+
+let card = getCard(pokemon);
+document.getElementsByClassName('evolution')[0].appendChild(card);
+
+if(pokemon.evolution['next-evolution']) {
+    for(const nextEvolution of pokemon.evolution['next-evolution']) {
+        let card = getCard(nextEvolution);
+        document.getElementsByClassName('evolution')[0].appendChild(card);
+
+        if(nextEvolution['next-evolution']){
+            for(const nextEvolution2 of nextEvolution['next-evolution']) {
+                let card = getCard(nextEvolution2);
+                document.getElementsByClassName('evolution')[0].appendChild(card);
+            }
+        }
+    }
+}
